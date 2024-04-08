d06f4897773f24601aaad782712549739cdf7dc3
Daniel Ise <ise.daniel@gmail.com>
2024-04-08 12:47:45 -0300

    log-levels solved

--- cpp/log-levels/log_levels.cpp
+++ cpp/log-levels/log_levels.cpp
@@ -3,13 +3,18 @@
 namespace log_line {
     std::string message(std::string line) {
         // return the message
+        auto index{line.find(": ") + 2};
+        return line.substr(index);
     }
 
     std::string log_level(std::string line) {
         // return the log level
+        auto start{line.find("[")+1};
+        auto end{line.find("]")-1};
+        return line.substr(start, end);
     }
 
     std::string reformat(std::string line) {
-        // return the reformatted message
+        return message(line) + " (" + log_level(line) + ")";
     }
 }
