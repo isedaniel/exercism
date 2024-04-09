9f3133029c76d6697088f9d5f6b7b951b385479b
Daniel Ise <ise.daniel@gmail.com>
2024-04-09 10:06:48 -0300

    raindrops solved

--- cpp/raindrops/raindrops.cpp
+++ cpp/raindrops/raindrops.cpp
@@ -3,7 +3,17 @@
 
 namespace raindrops {
     std::string convert(int i) {
-    return i;
+    std::string sound{};
+    if (i % 3 == 0)
+        sound += "Pling";
+    if (i % 5 == 0)
+        sound += "Plang";
+    if (i % 7 == 0)
+        sound += "Plong";
+
+    if (sound.empty())
+        return std::to_string(i);
+    return sound;
 }
 
 }  // namespace raindrops
