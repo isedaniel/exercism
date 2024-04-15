e978ca4f71a5ef85e1328d91f18d57cc95bc3e04
Daniel Ise <ise.daniel@gmail.com>
2024-04-15 14:13:57 -0300

    lasagna-master half done

--- cpp/lasagna-master/lasagna_master.cpp
+++ cpp/lasagna-master/lasagna_master.cpp
@@ -1,6 +1,27 @@
 #include "lasagna_master.h"
 
-namespace lasagna_master {
+#include <string>
+#include <vector>
 
+namespace Consts {
+int noodles{50};
+double sauce{.2};
+}
+
+namespace lasagna_master {
+int preparationTime(std::vector<std::string> layers, int time) {
+  return layers.size() * time;
+}
+
+amount quantities(std::vector<std::string> layers){
+  int noodles{};
+  int sauce{};
+  for (int i{}, size = layers.size(); i<size; ++i){
+    if (layers.at(i) == "noodles") ++noodles;
+    if (layers.at(i) == "sauce") ++sauce;
+  }
+
+  return amount{noodles * Consts::noodles, sauce * Consts::sauce};
+}
 
 }  // namespace lasagna_master
--- cpp/lasagna-master/lasagna_master.h
+++ cpp/lasagna-master/lasagna_master.h
@@ -1,10 +1,17 @@
 #pragma once
+#include <string>
+#include <vector>
 
 namespace lasagna_master {
 
 struct amount {
-    int noodles;
-    double sauce;
+  int noodles;
+  double sauce;
 };
 
+int preparationTime(std::vector<std::string> layers, int time = 2);
+amount quantities(std::vector<std::string> layers);
+
 }  // namespace lasagna_master
+
+
