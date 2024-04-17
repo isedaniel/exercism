fe1425c795c087278b8289bf1c4b85ca6717b0f3
Daniel Ise <ise.daniel@gmail.com>
2024-04-17 13:48:52 -0300

    squeaky-clean half done

--- java/squeaky-clean/src/main/java/SqueakyClean.java
+++ java/squeaky-clean/src/main/java/SqueakyClean.java
@@ -1,5 +1,22 @@
+import static java.util.Map.entry;
+
 class SqueakyClean {
+    public static Map<Integer, Character> leet = Map.ofEntries(
+            entry(0, 'o'),
+            entry(),
+    );
+
     static String clean(String identifier) {
-        throw new UnsupportedOperationException("Please implement the (static) SqueakyClean.clean() method");
+        String cleaned = "";
+        boolean upper = false;
+        for (int i=0;i < identifier.length(); i++){
+            char c= identifier.charAt(i);
+            if (c == ' ') { cleaned += '_'; }
+            else if (Character.isDigit(c)) {}
+            else if (c == '-') { upper = true; }
+            else if (upper) { upper = false; cleaned += Character.toUpperCase(c); }
+            else { cleaned += c; }
+        }
+        return cleaned;
     }
 }
