c8c607247c001b44f99ce441d57594f83b86dc9e
Daniel Ise <ise.daniel@gmail.com>
2024-04-18 11:35:10 -0300

    squeaky-clean done

--- java/squeaky-clean/src/main/java/SqueakyClean.java
+++ java/squeaky-clean/src/main/java/SqueakyClean.java
@@ -1,21 +1,31 @@
-import static java.util.Map.entry;
-
 class SqueakyClean {
-    public static Map<Integer, Character> leet = Map.ofEntries(
-            entry(0, 'o'),
-            entry(1, 'i'),
-    );
-
     static String clean(String identifier) {
         String cleaned = "";
         boolean upper = false;
-        for (int i=0;i < identifier.length(); i++){
-            char c= identifier.charAt(i);
-            if (c == ' ') { cleaned += '_'; }
-            else if (Character.isDigit(c)) {}
-            else if (c == '-') { upper = true; }
-            else if (upper) { upper = false; cleaned += Character.toUpperCase(c); }
-            else { cleaned += c; }
+        for (int i=0; i < identifier.length(); i++){
+            char c = identifier.charAt(i);
+            if (c == ' ') 
+                cleaned += '_';
+            else if (Character.isDigit(c)) {
+                if (c == '0')
+                    cleaned += 'o';
+                else if (c == '1')
+                    cleaned += 'l';
+                else if (c == '3')
+                    cleaned += 'e';
+                else if (c == '4')
+                    cleaned += 'a';
+                else if (c == '7')
+                    cleaned += 't';
+            }
+            else if (c == '-') 
+                upper = true;
+            else if (upper) {
+                upper = false; 
+                cleaned += Character.toUpperCase(c);
+            }
+            else if (Character.isLetter(c)) 
+                cleaned += c;
         }
         return cleaned;
     }
