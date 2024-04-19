065dd5a09a88be41ad7be30f0528d24cd9a99021
Daniel Ise <ise.daniel@gmail.com>
2024-04-19 12:44:29 -0300

    elons-toy-car done

--- java/elons-toy-car/src/main/java/ElonsToyCar.java
+++ java/elons-toy-car/src/main/java/ElonsToyCar.java
@@ -1,17 +1,25 @@
 public class ElonsToyCar {
+    private int distance;
+    private int battery = 100;
+
     public static ElonsToyCar buy() {
-        throw new UnsupportedOperationException("Please implement the (static) ElonsToyCar.buy()  method");
+        return new ElonsToyCar();
     }
 
     public String distanceDisplay() {
-        throw new UnsupportedOperationException("Please implement the ElonsToyCar.distanceDisplay()  method");
+        return "Driven " + this.distance + " meters";
     }
 
     public String batteryDisplay() {
-        throw new UnsupportedOperationException("Please implement the ElonsToyCar.batteryDisplay()  method");
+        if (this.battery > 0)
+            return "Battery at " + this.battery + "%";
+        return "Battery empty";
     }
 
     public void drive() {
-        throw new UnsupportedOperationException("Please implement the ElonsToyCar.drive()  method");
+        if (this.battery > 0) {
+            distance += 20;
+            battery--;
+        }
     }
 }
