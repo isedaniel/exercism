9d74ec010dc46ebc6f1b9236cded11e6c19393f5
Daniel Ise <ise.daniel@gmail.com>
2024-04-19 13:32:12 -0300

    need-for-speed done

--- java/need-for-speed/src/main/java/NeedForSpeed.java
+++ java/need-for-speed/src/main/java/NeedForSpeed.java
@@ -1,31 +1,45 @@
 class NeedForSpeed {
+    private int speed;
+    private int batteryDrain;
+    private int distanceDriven;
+    private int battery = 100;
+
     NeedForSpeed(int speed, int batteryDrain) {
-        throw new UnsupportedOperationException("Please implement the NeedForSpeed constructor");
+        this.speed = speed;
+        this.batteryDrain = batteryDrain;
     }
 
     public boolean batteryDrained() {
-        throw new UnsupportedOperationException("Please implement the NeedForSpeed.batteryDrained() method");
+        return this.battery < batteryDrain;
     }
 
     public int distanceDriven() {
-        throw new UnsupportedOperationException("Please implement the NeedForSpeed.distanceDriven() method");
+        return this.distanceDriven;
     }
 
     public void drive() {
-        throw new UnsupportedOperationException("Please implement the NeedForSpeed.drive() method");
+        if (!batteryDrained()){
+            this.distanceDriven += this.speed;
+            this.battery -= batteryDrain;
+        } 
     }
 
     public static NeedForSpeed nitro() {
-        throw new UnsupportedOperationException("Please implement the (static) NeedForSpeed.nitro() method");
+        return new NeedForSpeed(50, 4);
     }
 }
 
 class RaceTrack {
+    private int distance;
+
     RaceTrack(int distance) {
-        throw new UnsupportedOperationException("Please implement the RaceTrack constructor");
+        this.distance = distance;
     }
 
     public boolean tryFinishTrack(NeedForSpeed car) {
-        throw new UnsupportedOperationException("Please implement the RaceTrack.tryFinishTrack() method");
+        while (!car.batteryDrained()) {
+            car.drive();
+        }
+        return car.distanceDriven() >= this.distance;
     }
 }
