4bcce8cecb7cf5b8f8481de8d2ff709fd13c9b3a
Daniel Ise <ise.daniel@gmail.com>
2024-04-23 11:28:37 -0300

    tim-from-marketing done

--- java/tim-from-marketing/src/main/java/Badge.java
+++ java/tim-from-marketing/src/main/java/Badge.java
@@ -1,5 +1,7 @@
 class Badge {
     public String print(Integer id, String name, String department) {
-        throw new UnsupportedOperationException("Please implement the Badge.print() method");
+        String id_str = (id == null)? "" : "[" + id + "] - ";
+        String dp_str = (department == null)? "OWNER" : department.toUpperCase();
+        return  id_str + name + " - " + dp_str;
     }
 }
