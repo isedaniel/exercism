8248abb92a18b5e09b403f275851b120fe3314a3
Daniel Ise <ise.daniel@gmail.com>
2024-04-23 12:49:41 -0300

    booking-up-for-beauty done

--- java/booking-up-for-beauty/src/main/java/AppointmentScheduler.java
+++ java/booking-up-for-beauty/src/main/java/AppointmentScheduler.java
@@ -1,24 +1,33 @@
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 class AppointmentScheduler {
     public LocalDateTime schedule(String appointmentDateDescription) {
-        throw new UnsupportedOperationException("Please implement the AppointmentScheduler.schedule() method");
+        DateTimeFormatter parser = DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss");
+        return LocalDateTime.parse(appointmentDateDescription, parser);
     }
 
     public boolean hasPassed(LocalDateTime appointmentDate) {
-        throw new UnsupportedOperationException("Please implement the AppointmentScheduler.hasPassed() method");
+        return LocalDateTime.now().isAfter(appointmentDate);
     }
 
     public boolean isAfternoonAppointment(LocalDateTime appointmentDate) {
-        throw new UnsupportedOperationException("Please implement the AppointmentScheduler.isAfternoonAppointment() method");
+        return (12 <= appointmentDate.getHour()) && (appointmentDate.getHour() < 18);
     }
 
     public String getDescription(LocalDateTime appointmentDate) {
-        throw new UnsupportedOperationException("Please implement the AppointmentScheduler.getDescription() method");
+        // Friday, March 29, 2019
+        DateTimeFormatter datePrinter = DateTimeFormatter.ofPattern("EEEE, MMMM d, yyyy,");
+        // 3:00 PM.
+        DateTimeFormatter hourPrinter = DateTimeFormatter.ofPattern("h:mm a.");
+        return "You have an appointment on " +
+            appointmentDate.format(datePrinter) +
+            " at " +
+            appointmentDate.format(hourPrinter);
     }
 
     public LocalDate getAnniversaryDate() {
-        throw new UnsupportedOperationException("Please implement the AppointmentScheduler.getAnniversaryDate() method");
+        return LocalDate.of(LocalDate.now().getYear(), 9, 15);
     }
 }
