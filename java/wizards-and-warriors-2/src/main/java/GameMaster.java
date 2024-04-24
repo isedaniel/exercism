ea2b27c8e41a0c28a938deec1c2d4ae61b167087
Daniel Ise <ise.daniel@gmail.com>
2024-04-24 13:55:54 -0300

    wizards-and-warriors-2 done

--- java/wizards-and-warriors-2/src/main/java/GameMaster.java
+++ java/wizards-and-warriors-2/src/main/java/GameMaster.java
@@ -1,12 +1,26 @@
 public class GameMaster {
 
-    // TODO: define a 'describe' method that returns a description of a Character
+    public String describe(Character c) {
+        return "You're a level " + c.getLevel() + " " + 
+            c.getCharacterClass() + " with " + c.getHitPoints() + " hit points.";
+    }
 
-    // TODO: define a 'describe' method that returns a description of a Destination
+    public String describe(Destination d) {
+        return "You've arrived at " + d.getName() + ", which has " +
+            d.getInhabitants() + " inhabitants.";
+    }
 
-    // TODO: define a 'describe' method that returns a description of a TravelMethod
+    public String describe(TravelMethod t) {
+        String method = (t == TravelMethod.WALKING)? "by walking." : "on horseback.";
+        return "You're traveling to your destination " + method;
+    }
 
-    // TODO: define a 'describe' method that returns a description of a Character, Destination and TravelMethod
 
-    // TODO: define a 'describe' method that returns a description of a Character and Destination
+    public String describe(Character c, Destination d, TravelMethod t) {
+        return describe(c) + " " + describe(t) + " " + describe(d);
+    }
+
+    public String describe(Character c, Destination d) {
+        return describe(c) + " " + describe(TravelMethod.WALKING) + " " + describe(d);
+    }
 }
