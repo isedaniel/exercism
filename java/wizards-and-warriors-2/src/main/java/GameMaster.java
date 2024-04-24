a4708ebacd6eaae03ee9a6753ca5935bb112f0a0
Daniel Ise <ise.daniel@gmail.com>
2024-04-24 14:10:08 -0300

    wizards-and-warriors-2 refactored

--- java/wizards-and-warriors-2/src/main/java/GameMaster.java
+++ java/wizards-and-warriors-2/src/main/java/GameMaster.java
@@ -1,26 +1,33 @@
 public class GameMaster {
 
     public String describe(Character c) {
-        return "You're a level " + c.getLevel() + " " + 
-            c.getCharacterClass() + " with " + c.getHitPoints() + " hit points.";
+        return String.format("You're a level %d %s with %d hit points.",
+                c.getLevel(),
+                c.getCharacterClass(),
+                c.getHitPoints());
     }
 
     public String describe(Destination d) {
-        return "You've arrived at " + d.getName() + ", which has " +
-            d.getInhabitants() + " inhabitants.";
+        return String.format("You've arrived at %s, which has %d inhabitants.",
+                d.getName(),
+                d.getInhabitants());
     }
 
     public String describe(TravelMethod t) {
-        String method = (t == TravelMethod.WALKING)? "by walking." : "on horseback.";
-        return "You're traveling to your destination " + method;
+        String method = (t == TravelMethod.WALKING)? "by walking" : "on horseback";
+        return String.format("You're traveling to your destination %s.",
+                method);
     }
 
 
     public String describe(Character c, Destination d, TravelMethod t) {
-        return describe(c) + " " + describe(t) + " " + describe(d);
+        return String.format("%s %s %s",
+                this.describe(c),
+                this.describe(t),
+                this.describe(d));
     }
 
     public String describe(Character c, Destination d) {
-        return describe(c) + " " + describe(TravelMethod.WALKING) + " " + describe(d);
+        return this.describe(c, d, TravelMethod.WALKING);
     }
 }
