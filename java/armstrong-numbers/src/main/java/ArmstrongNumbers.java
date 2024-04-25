b7571a9e32cc60be599bc678d02d8c1ddbc56b27
Daniel Ise <ise.daniel@gmail.com>
2024-04-25 14:32:36 -0300

    ArmstrongNumbers redoing

--- java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
+++ java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -1,13 +1,22 @@
 class ArmstrongNumbers {
 
     boolean isArmstrongNumber(int number) {
-        char[] numbers = Integer.toString(number).toCharArray();
-        int size = numbers.length;
-        int armstrong = 0;
-        for (var n : numbers) {
-            armstrong += Math.pow(Character.getNumericValue(n), size);
+        int sum = 0;
+        int digits = (int) Math.log10(number) + 1;
+        for (int i=1,n=number; i<=digits; i++){
+            sum += (int)Math.pow(n % (10*i), digits);
+            n -= n % (10*i);
         }
-        return armstrong == number;
+        return sum == number;
     }
 
+    int intArmstrongNumber(int number) {
+        int sum = 0;
+        int digits = (int) Math.log10(number) + 1;
+        for (int i=1,n=number; i<=digits; i++){
+            sum += (int)Math.pow(n % (10*i), digits);
+            n -= n % (10*i);
+        }
+        return sum;
+    }
 }
