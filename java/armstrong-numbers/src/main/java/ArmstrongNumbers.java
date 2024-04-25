4b5b7e368362ac2db113488f6ec5868057086d21
Daniel Ise <ise.daniel@gmail.com>
2024-04-25 17:57:33 -0300

    armstrong-numbers finished

--- java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
+++ java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -2,7 +2,7 @@ class ArmstrongNumbers {
     boolean isArmstrongNumber(int number) {
         int sum = 0;
         int digits = (int) Math.log10(number) + 1;
-        for (int i=1,n=number; i<=digits; i++){
+        for (int i=0, n=number; i < digits; i++){
             int digit = n % 10;
             sum += Math.pow(digit, digits);
             n = n / 10;
