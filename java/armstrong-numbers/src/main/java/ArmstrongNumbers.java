58a3f9a0da25588d03d5382d44553680eb58d694
Daniel Ise <ise.daniel@gmail.com>
2024-04-25 17:41:41 -0300

    armstrong-numbers refactored?

--- java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
+++ java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -1,22 +1,12 @@
 class ArmstrongNumbers {
-
     boolean isArmstrongNumber(int number) {
         int sum = 0;
         int digits = (int) Math.log10(number) + 1;
         for (int i=1,n=number; i<=digits; i++){
-            sum += (int)Math.pow(n % (10*i), digits);
-            n -= n % (10*i);
+            int digit = n % 10;
+            sum += Math.pow(digit, digits);
+            n = n / 10;
         }
         return sum == number;
-    }
-
-    int intArmstrongNumber(int number) {
-        int sum = 0;
-        int digits = (int) Math.log10(number) + 1;
-        for (int i=1,n=number; i<=digits; i++){
-            sum += (int)Math.pow(n % (10*i), digits);
-            n -= n % (10*i);
-        }
-        return sum;
     }
 }
