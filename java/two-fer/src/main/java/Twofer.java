5f0ba50c1c84b7168897bbd5b3d0d41ee157a675
Daniel Ise <ise.daniel@gmail.com>
2024-04-25 13:51:08 -0300

    armstrong-numbers started

--- java/two-fer/src/main/java/Twofer.java
+++ java/two-fer/src/main/java/Twofer.java
@@ -1,6 +1,6 @@
 public class Twofer {
     public String twofer(String name) {
-        return String.format("One for %s, one for me.",
-                (name == null)? "you" : name);
+        name = (name == null)? "you" : name;
+        return "One for " + name + ", one for me.";
     }
 }
