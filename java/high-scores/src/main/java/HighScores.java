1dc79c98144c55b41be35765af918e10b774b3df
Daniel Ise <ise.daniel@gmail.com>
2024-04-28 20:07:28 -0300

    high-scores half done

--- java/high-scores/src/main/java/HighScores.java
+++ java/high-scores/src/main/java/HighScores.java
@@ -1,25 +1,70 @@
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 class HighScores {
+    private List<Integer> scores;
+    private int[] sorted;
 
     public HighScores(List<Integer> highScores) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.scores = highScores;
+        int[] sort = highScores.stream().mapToInt(i->i).toArray();
+        quickSort(sort, 0, sort.length - 1);
+        this.sorted = sort;
     }
 
     List<Integer> scores() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return this.scores;
     }
 
     Integer latest() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return this.scores.get(this.scores.size() - 1);
     }
 
     Integer personalBest() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        int best = 0;
+        for (var score : this.scores) {
+            if (score > best) best = score;
+        }
+        return best;
+    }
+
+    void quickSort(int[] arr, int start, int end) {
+        // base case
+        if (end <= start) return;
+
+        int pivot = partition(arr, start, end); 
+        quickSort(arr, start, pivot - 1);
+        quickSort(arr, pivot + 1, end);
+    }
+
+    int partition(int[] arr, int start, int end) {
+        int pivot = arr[end];
+        int i = start - 1;
+
+        for (int j = start; j < end; j++){
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        i++;
+        int temp = arr[i];
+        arr[i] = arr[end];
+        arr[end] = temp;
+
+        return i;
     }
 
     List<Integer> personalTopThree() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        int[] topThree = new int[3];
+        for (int i=this.sorted.length - 1, j=0; i > this.sorted.length - 4; i--){
+            topThree[j] = this.sorted[i];
+            j++;
+        }
+        return Arrays.stream(topThree).boxed().toList();
     }
 
 }
