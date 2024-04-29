520a93933d26e4fdf8312522777ddfb30f84ae08
Daniel Ise <ise.daniel@gmail.com>
2024-04-29 14:05:37 -0300

    resistor-color done

--- java/resistor-color/src/main/java/ResistorColor.java
+++ java/resistor-color/src/main/java/ResistorColor.java
@@ -1,9 +1,12 @@
+import java.util.Arrays; 
+
 class ResistorColor {
     int colorCode(String color) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return Arrays.asList(colors()).indexOf(color);
     }
 
     String[] colors() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return new String[]{"black", "brown", "red", "orange", "yellow", 
+            "green", "blue", "violet", "grey", "white"};
     }
 }
