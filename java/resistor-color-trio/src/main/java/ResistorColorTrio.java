8fb4b653430fd388c10ebd4e28841e1e2abc39f7
Daniel Ise <ise.daniel@gmail.com>
2024-04-30 09:56:13 -0300

    resistor-color-trio done

--- java/resistor-color-trio/src/main/java/ResistorColorTrio.java
+++ java/resistor-color-trio/src/main/java/ResistorColorTrio.java
@@ -1,5 +1,23 @@
 class ResistorColorTrio {
+    enum Colors {
+        black, brown, red, orange, yellow, green, blue, violet, grey, white
+    }
+
     String label(String[] colors) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        long resistance = (Colors.valueOf(colors[0]).ordinal() * 10 +
+            Colors.valueOf(colors[1]).ordinal()) *
+            (long) Math.pow(10, Colors.valueOf(colors[2]).ordinal());
+
+        if (resistance > 1_000_000_000) {
+            return (resistance /= 1_000_000_000) + " gigaohms";
+        }
+        else if (resistance > 1_000_000) {
+            return (resistance /= 1_000_000) + " megaohms";
+        }
+        else if (resistance > 1000) {
+            return (resistance /= 1_000) + " kiloohms";
+        }
+
+        return resistance + " ohms";
     }
 }
