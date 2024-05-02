4aed18fa869db957cc9a296311ac6c41d26cf8f6
Daniel Ise <ise.daniel@gmail.com>
2024-05-02 14:04:01 -0300

    gigasecond refactored

--- java/gigasecond/src/main/java/Gigasecond.java
+++ java/gigasecond/src/main/java/Gigasecond.java
@@ -1,19 +1,18 @@
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 
 public class Gigasecond {
-    private LocalDateTime time;
+    private LocalDateTime dateTime;
 
     public Gigasecond(LocalDate moment) {
-        this.time = LocalDateTime.of(moment, LocalTime.of(0, 0)).plusSeconds(1_000_000_000);
+        this.dateTime = moment.atTime(0, 0).plusSeconds(1_000_000_000);
     }
 
     public Gigasecond(LocalDateTime moment) {
-        this.time = moment.plusSeconds(1_000_000_000);
+        this.dateTime = moment.plusSeconds(1_000_000_000);
     }
 
     public LocalDateTime getDateTime() {
-        return time;
+        return dateTime;
     }
 }
