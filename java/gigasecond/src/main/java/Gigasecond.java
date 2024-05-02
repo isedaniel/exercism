c3756344ab90cf857ad3673338f42e8eb68aeb8e
Daniel Ise <ise.daniel@gmail.com>
2024-05-02 13:24:39 -0300

    gigasecond done

--- java/gigasecond/src/main/java/Gigasecond.java
+++ java/gigasecond/src/main/java/Gigasecond.java
@@ -1,16 +1,19 @@
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class Gigasecond {
+    private LocalDateTime time;
+
     public Gigasecond(LocalDate moment) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.time = LocalDateTime.of(moment, LocalTime.of(0, 0)).plusSeconds(1_000_000_000);
     }
 
     public Gigasecond(LocalDateTime moment) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.time = moment.plusSeconds(1_000_000_000);
     }
 
     public LocalDateTime getDateTime() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return time;
     }
 }
