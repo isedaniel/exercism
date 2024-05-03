b8476f6b881aa381b5dc9ffd330d617bcfc9ecba
Daniel Ise <ise.daniel@gmail.com>
2024-05-03 12:45:23 -0300

    hamming done

--- java/hamming/src/main/java/Hamming.java
+++ java/hamming/src/main/java/Hamming.java
@@ -1,9 +1,18 @@
 public class Hamming {
+    private int hammingDistance;
+
     public Hamming(String leftStrand, String rightStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (leftStrand.length() != rightStrand.length())
+            throw new IllegalArgumentException("strands must be of equal length");
+
+        for(int i=0; i<leftStrand.length(); i++){
+            if (leftStrand.charAt(i) != rightStrand.charAt(i))
+                hammingDistance++;
+        }
+
     }
 
     public int getHammingDistance() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return hammingDistance;
     }
 }
