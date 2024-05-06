bb9b7c815a20c2ecec7725c70004d06a2ffd16a2
Daniel Ise <ise.daniel@gmail.com>
2024-05-06 12:04:48 -0300

    perfect-numbers refactored

--- java/perfect-numbers/src/main/java/NaturalNumber.java
+++ java/perfect-numbers/src/main/java/NaturalNumber.java
@@ -1,5 +1,4 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.stream.IntStream;
 
 class NaturalNumber {
     Classification classification;
@@ -8,16 +7,10 @@ class NaturalNumber {
         if (number < 1)
             throw new IllegalArgumentException("You must supply a natural number (positive integer)");
 
-        Set<Integer> factors = new HashSet<>();
-        int step = (number % 2 == 0) ? 1 : 2;
-        for (int i=1; i<=Math.sqrt(number); i+=step){
-            if (number % i == 0) {
-                factors.add(i);
-                factors.add(number / i);
-            }
-        }
-
-        int aliquot = factors.stream().mapToInt(Integer::intValue).sum() - number;
+        int aliquot = IntStream.rangeClosed(1, number /2)
+            .parallel()
+            .filter(i -> number % i == 0)
+            .sum();
 
         if (aliquot < number) 
             this.classification = Classification.DEFICIENT;
