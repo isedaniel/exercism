d8f2efff1f15da932e02ae3c1c2be77e58e8b826
Daniel Ise <ise.daniel@gmail.com>
2024-05-06 11:50:18 -0300

    perfect-numbers first iteration

--- java/perfect-numbers/src/main/java/NaturalNumber.java
+++ java/perfect-numbers/src/main/java/NaturalNumber.java
@@ -1,9 +1,33 @@
+import java.util.HashSet;
+import java.util.Set;
+
 class NaturalNumber {
+    Classification classification;
+
     NaturalNumber(int number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation");
+        if (number < 1)
+            throw new IllegalArgumentException("You must supply a natural number (positive integer)");
+
+        Set<Integer> factors = new HashSet<>();
+        int step = (number % 2 == 0) ? 1 : 2;
+        for (int i=1; i<=Math.sqrt(number); i+=step){
+            if (number % i == 0) {
+                factors.add(i);
+                factors.add(number / i);
+            }
+        }
+
+        int aliquot = factors.stream().mapToInt(Integer::intValue).sum() - number;
+
+        if (aliquot < number) 
+            this.classification = Classification.DEFICIENT;
+        else if (aliquot > number)
+            this.classification = Classification.ABUNDANT;
+        else
+            this.classification = Classification.PERFECT;
     }
 
     Classification getClassification() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return this.classification;
     }
 }
