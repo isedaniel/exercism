5e19ad3ae45b6bdc8c2aec23164ef52c6aa3351a
Daniel Ise <ise.daniel@gmail.com>
2024-05-07 12:28:28 -0300

    raindrops done

--- java/raindrops/src/main/java/RaindropConverter.java
+++ java/raindrops/src/main/java/RaindropConverter.java
@@ -1,7 +1,16 @@
 class RaindropConverter {
 
     String convert(int number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        String sound = "";
+        if (number % 3 == 0) 
+            sound += "Pling";
+        if (number % 5 == 0) 
+            sound += "Plang";
+        if (number % 7 == 0) 
+            sound += "Plong";
+
+        if (sound != "") return sound;
+        return "" + number;
     }
 
 }
