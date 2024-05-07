159416c51731ad2d273f4319f5efe224b9f528de
Daniel Ise <ise.daniel@gmail.com>
2024-05-07 13:45:25 -0300

    say halfdone

--- java/say/src/main/java/Say.java
+++ java/say/src/main/java/Say.java
@@ -1,6 +1,22 @@
+import java.util.HashMap;
+
 public class Say {
+    private static HashMap<Long, String> numbers = new HashMap<>();
+    static {
+        numbers.put(0L, "zero");
+        numbers.put(1L, "one");
+        numbers.put(14L, "fourteen");
+        numbers.put(20L, "twenty");
+    }
 
     public String say(long number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        StringBuilder numberName;
+
+        while (number > 0) {
+            long digit = number % 10;
+
+        }
+        return numbers.get(number);
+
     }
 }
