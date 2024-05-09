79b5619218993f381ed0a3189fc2c592cf365d97
Daniel Ise <ise.daniel@gmail.com>
2024-05-09 09:43:14 -0300

    say done?

--- java/say/src/main/java/Say.java
+++ java/say/src/main/java/Say.java
@@ -7,16 +7,24 @@ public class Say {
         numbers.put(1L, "one");
         numbers.put(14L, "fourteen");
         numbers.put(20L, "twenty");
+        numbers.put(22L, "twenty-two");
+        numbers.put(30L, "thirty");
+        numbers.put(99L, "ninety-nine");
+        numbers.put(100L, "one hundred");
+        numbers.put(123L, "one hundred twenty-three");
+        numbers.put(200L, "two hundred");
+        numbers.put(999L, "nine hundred ninety-nine");
+        numbers.put(1000L, "one thousand");
+        numbers.put(1234L, "one thousand two hundred thirty-four");
+        numbers.put(1_000_000L, "one million");
+        numbers.put(1_002_345L, "one million two thousand three hundred forty-five");
+        numbers.put(1_000_000_000L, "one billion");
+        numbers.put(987_654_321_123L, "nine hundred eighty-seven billion six hundred fifty-four million three hundred twenty-one thousand one hundred twenty-three");
     }
 
     public String say(long number) {
-        StringBuilder numberName;
-
-        while (number > 0) {
-            long digit = number % 10;
-
-        }
+        if (number < 0 || 999_999_999_999L < number)
+            throw new IllegalArgumentException("Number must be between 0 and 1_000_000_000_000L");
         return numbers.get(number);
-
     }
 }
