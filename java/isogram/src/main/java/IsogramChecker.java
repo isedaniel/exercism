49b7361c454ead221a56ac7e7a940858fc925800
Daniel Ise <ise.daniel@gmail.com>
2024-05-13 13:24:07 -0300

    isogram refactored

--- java/isogram/src/main/java/IsogramChecker.java
+++ java/isogram/src/main/java/IsogramChecker.java
@@ -1,19 +1,12 @@
-import java.util.Collections;
 import java.util.HashSet;
-import java.util.Set;
 
 class IsogramChecker {
 
     boolean isIsogram(String phrase) {
-        if (phrase == "") return true;
-
-        phrase = phrase.replace("-", "")
-            .replace(" ", "").toLowerCase();
-
-        Set<String> letters = new HashSet<String>();
-        Collections.addAll(letters, phrase.split(""));
-
-        return (letters.size() == phrase.length());
+        return phrase.chars()
+            .filter(Character::isLetter)
+            .map(Character::toLowerCase)
+            .allMatch(new HashSet<>()::add);
     }
 
 }
