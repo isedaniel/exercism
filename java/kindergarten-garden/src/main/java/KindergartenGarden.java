1c899478be25cc91f9a3775b08640f6b817cbc0b
Daniel Ise <ise.daniel@gmail.com>
2024-05-14 14:05:10 -0300

    kindergarten-garden halfdone

--- java/kindergarten-garden/src/main/java/KindergartenGarden.java
+++ java/kindergarten-garden/src/main/java/KindergartenGarden.java
@@ -1,13 +1,28 @@
+import java.util.ArrayList;
 import java.util.List;
 
 class KindergartenGarden {
+    final private String row1;
+    final private String row2;
 
     KindergartenGarden(String garden) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.row1 = garden.split("\n")[0];
+        this.row2 = garden.split("\n")[1];
     }
 
     List<Plant> getPlantsOfStudent(String student) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        List<Plant> plants = new ArrayList<>();
+        int offset = student.charAt(0) - 'A';
+
+        for (int i=offset; i<offset+2; i++) {
+            plants.add(Plant.getPlant(row1.charAt(i)));
+        }
+        
+        for (int i=offset; i<offset+2; i++) {
+            plants.add(Plant.getPlant(row2.charAt(i)));
+        }
+
+        return plants;
     }
 
 }
