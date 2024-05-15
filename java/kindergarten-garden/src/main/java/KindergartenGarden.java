a8d0df6f774c36130b954aa7d29e1684304fcf9e
Daniel Ise <ise.daniel@gmail.com>
2024-05-15 11:40:16 -0300

    kindergaten-garden refactored

--- java/kindergarten-garden/src/main/java/KindergartenGarden.java
+++ java/kindergarten-garden/src/main/java/KindergartenGarden.java
@@ -1,17 +1,27 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 class KindergartenGarden {
     final private String[] rows;
+    final private List<String> students;
 
     KindergartenGarden(String garden) {
         this.rows = garden.split("\n");
+        this.students = Arrays.asList("Alice", "Bob", "Charlie",
+            "David", "Eve", "Fred", "Ginny", "Harriet", 
+            "Ileana", "Joseph", "Kincaid", "Larry");
+    }
+
+    KindergartenGarden(String garden, List<String> students) {
+        this.rows = garden.split("\n");
+        this.students = students;
     }
 
     List<Plant> getPlantsOfStudent(String student) {
         List<Plant> plants = new ArrayList<>();
 
-        int index = 2 * (student.charAt(0) - 'A');
+        int index = 2 * students.indexOf(student);
         for (String row : rows) {
             plants.add(Plant.getPlant(row.charAt(index)));
             plants.add(Plant.getPlant(row.charAt(index + 1)));
