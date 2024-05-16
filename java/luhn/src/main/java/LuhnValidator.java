09ce15e806e21c042f5572a6e8e8b0a29cf3df4e
Daniel Ise <ise.daniel@gmail.com>
2024-05-16 12:39:42 -0300

    luhn done

--- java/luhn/src/main/java/LuhnValidator.java
+++ java/luhn/src/main/java/LuhnValidator.java
@@ -1,7 +1,27 @@
 class LuhnValidator {
 
-    boolean isValid(String candidate) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    boolean isValid(String number) {
+        number = new StringBuilder(number)
+            .reverse()
+            .toString()
+            .replace(" ", "");
+
+        if (!number.matches("[0-9]+") || number.equals("0"))
+            return false;
+
+        boolean even = false;
+        int sum = 0;
+        for (char c : number.toCharArray()) {
+            int n = Character.getNumericValue(c);
+
+            if (even) {
+                n = (n*2 > 9) ? (n*2 - 9) : (n*2);
+            }
+            even = !even;
+
+            sum += n;
+        }
+        return (sum % 10 == 0);
     }
 
 }
