d71e2ab77e1ec0d255712e787bb25421e215d70a
Daniel Ise <ise.daniel@gmail.com>
2024-05-17 13:12:57 -0300

    nth-prime done

--- java/nth-prime/src/main/java/PrimeCalculator.java
+++ java/nth-prime/src/main/java/PrimeCalculator.java
@@ -1,7 +1,24 @@
 class PrimeCalculator {
 
     int nth(int nth) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (nth < 1)
+            throw new IllegalArgumentException();
+
+        int n = 2;
+        int primes = 1;
+        while (primes<nth) {
+            n++;
+            if (isPrime(n)) primes++; 
+        }
+        return n;
+    }
+
+    boolean isPrime(int n) {
+        for (int i=2; i<=Math.sqrt(n); i++) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
     }
 
 }
