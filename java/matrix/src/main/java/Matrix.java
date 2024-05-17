b3ab588ee80b0aa74adfc52d1735b2313cdcec24
Daniel Ise <ise.daniel@gmail.com>
2024-05-17 12:07:40 -0300

    matrix done

--- java/matrix/src/main/java/Matrix.java
+++ java/matrix/src/main/java/Matrix.java
@@ -1,15 +1,36 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 class Matrix {
+    private List<List<Integer>> rows = new ArrayList<>();
+    private List<List<Integer>> columns = new ArrayList<>();
 
-    Matrix(String matrixAsString) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    Matrix(String stringMatrix) {
+        for (String stringRow : stringMatrix.split("\n")) {
+            List<Integer> row = new ArrayList<>(Arrays.asList(stringRow.split(" ")))
+                .stream()
+                .map(v -> Integer.parseInt(v))
+                .toList();
+
+            this.rows.add(row);
+        }
+
+        for (int i=0; i<this.rows.get(0).size(); i++) {
+            List<Integer> column = new ArrayList<>();
+            for (int j=0; j<this.rows.size(); j++) {
+                
+                column.add(this.rows.get(j).get(i));
+            }
+            this.columns.add(column);
+        }
     }
 
-    int[] getRow(int rowNumber) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    int[] getRow(int row) {
+        return this.rows.get(row - 1).stream().mapToInt(Integer::intValue).toArray();
     }
 
-    int[] getColumn(int columnNumber) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    int[] getColumn(int column) {
+        return this.columns.get(column - 1).stream().mapToInt(Integer::intValue).toArray();
     }
 }
