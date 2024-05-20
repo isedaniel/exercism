b0341eaa0762800ba7ada1ffd990eebf3aac1ecf
Daniel Ise <ise.daniel@gmail.com>
2024-05-20 13:36:42 -0300

    rotational-cipher done

--- java/rotational-cipher/src/main/java/RotationalCipher.java
+++ java/rotational-cipher/src/main/java/RotationalCipher.java
@@ -1,11 +1,23 @@
 class RotationalCipher {
+    private int key;
 
     RotationalCipher(int shiftKey) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.key = shiftKey;
     }
 
     String rotate(String data) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        StringBuilder sb = new StringBuilder();
+        for (char c : data.toCharArray()) {
+            if ('a' <= c && c <= 'z')
+                sb.append(Character.toChars(
+                        (c - 'a' + this.key) % 26 + 'a'));
+            else if ('A' <= c && c <= 'Z')
+                sb.append(Character.toChars(
+                        (c - 'A' + this.key) % 26 + 'A'));
+            else
+                sb.append(c);
+        }
+        return sb.toString();
     }
 
 }
