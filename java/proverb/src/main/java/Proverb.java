1b08a74258e7c3769c308c11d6f1dd87e0e59abf
Daniel Ise <ise.daniel@gmail.com>
2024-05-20 10:51:09 -0300

    proverb done

--- java/proverb/src/main/java/Proverb.java
+++ java/proverb/src/main/java/Proverb.java
@@ -1,11 +1,30 @@
 class Proverb {
+    String[] words;
 
     Proverb(String[] words) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.words = words;
     }
 
     String recite() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (words.length == 0) return "";
+
+        if (words.length == 1) 
+            return "And all for the want of a "
+                + words[0]
+                + ".";
+
+        String proverb = "";
+        for (int i=1; i<words.length; i++) {
+            proverb += "For want of a " 
+                + words[i - 1]
+                + " the "
+                + words[i]
+                + " was lost.\n";
+        }
+        return proverb 
+            + "And all for the want of a "
+            + words[0]
+            + ".";
     }
 
 }
