3e51c8ecc7149158b3cc80e8e742cfcd56a77bf2
Daniel Ise <ise.daniel@gmail.com>
2024-05-21 13:48:01 -0300

    saddle-points half done

--- java/saddle-points/src/main/java/Matrix.java
+++ java/saddle-points/src/main/java/Matrix.java
@@ -2,12 +2,13 @@ import java.util.List;
 import java.util.Set;
 
 class Matrix {
+    final List<List<Integer>> values;
 
     Matrix(List<List<Integer>> values) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.values = values;
     }
 
     Set<MatrixCoordinate> getSaddlePoints() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        values.stream();
     }
 }
