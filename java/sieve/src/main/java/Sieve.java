2a1300e8155c77e837f19e5d9983ca02f1df862f
Daniel Ise <ise.daniel@gmail.com>
2024-05-22 12:50:45 -0300

    sieve half done

--- java/sieve/src/main/java/Sieve.java
+++ java/sieve/src/main/java/Sieve.java
@@ -1,12 +1,34 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 
 class Sieve {
-    private int max;
+    private boolean[] isPrime;
+    private List<Integer> primes;
 
-    Sieve(int maxPrime) {
-        this.max = maxPrime;
+    Sieve(int max) {
+        if (max < 2) {
+            primes = new ArrayList<>();
+            return;
+        }
+
+        isPrime = new boolean[max - 1];
+        Arrays.fill(isPrime, true);
+
+        for (int i=2; i <= Math.sqrt(max); i++) {
+            for (int j=i*i; j < max; j+=i) {
+                isPrime[j] = false;
+            }
+        }
+
+        primes = IntStream.range(2, max)
+            .filter(i -> isPrime[i])
+            .boxed()
+            .toList();
     }
 
     List<Integer> getPrimes() {
+        return this.primes;
     }
 }
