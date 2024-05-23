cfe4f9f84fd339dc0cda26c13a43a9dbf28073ca
Daniel Ise <ise.daniel@gmail.com>
2024-05-23 12:11:54 -0300

    sum-of-multiples refactor

--- java/sum-of-multiples/src/main/java/SumOfMultiples.java
+++ java/sum-of-multiples/src/main/java/SumOfMultiples.java
@@ -4,11 +4,11 @@ import java.util.stream.IntStream;
 class SumOfMultiples {
     private final int sum;
 
-    SumOfMultiples(int max, int[] numbers) {
+    SumOfMultiples(int max, int[] factors) {
         this.sum = IntStream.range(1, max)
-            .filter(number -> Arrays.stream(numbers)
-                    .anyMatch(divisor -> 
-                        (divisor > 0) && (number % divisor == 0)))
+            .filter(number -> Arrays.stream(factors)
+                    .anyMatch(factor -> 
+                        (factor > 0) && (number % factor == 0)))
             .sum();
     }
 
