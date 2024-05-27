46bee230e68280c8bf0670a6e6dc9424c5c54f5d
Daniel Ise <ise.daniel@gmail.com>
2024-05-27 14:00:55 -0300

    variable-length-quantity incoming

--- java/variable-length-quantity/src/main/java/VariableLengthQuantity.java
+++ java/variable-length-quantity/src/main/java/VariableLengthQuantity.java
@@ -3,7 +3,7 @@ import java.util.List;
 class VariableLengthQuantity {
 
     List<String> encode(List<Long> numbers) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        
     }
 
     List<String> decode(List<Long> bytes) {
