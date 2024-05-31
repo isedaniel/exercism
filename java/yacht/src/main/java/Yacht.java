b39711e9a2354f7d2039b7e9b514b0ba5062632a
Daniel Ise <ise.daniel@gmail.com>
2024-05-31 12:42:13 -0300

    yacht refactored

--- java/yacht/src/main/java/Yacht.java
+++ java/yacht/src/main/java/Yacht.java
@@ -1,65 +1,53 @@
 import java.util.Arrays;
 
 class Yacht {
+    private final int[] FULL_HOUSE_FREQ = new int[]{0,0,0,0,2,3};
     private int score;
 
     Yacht(int[] dice, YachtCategory yachtCategory) {
-        if (yachtCategory == YachtCategory.YACHT) {
-            this.score = yacht(dice);
-        }
-        else if (yachtCategory == YachtCategory.ONES) {
-            this.score = count(dice, 1);
-        }
-        else if (yachtCategory == YachtCategory.TWOS) {
-            this.score = count(dice, 2) * 2;
-        }
-        else if (yachtCategory == YachtCategory.THREES) {
-            this.score = count(dice, 3) * 3;
-        }
-        else if (yachtCategory == YachtCategory.FOURS) {
-            this.score = count(dice, 4) * 4;
-        }
-        else if (yachtCategory == YachtCategory.FIVES) {
-            this.score = count(dice, 5) * 5;
-        }
-        else if (yachtCategory == YachtCategory.SIXES) {
-            this.score = count(dice, 6) * 6;
-        }
-        else if (yachtCategory == YachtCategory.FULL_HOUSE) {
-            int[] scores = new int[6];
-            int sum = 0;
-            for (int i=1; i<=6; i++) {
-                scores[i-1] = count(dice, i);
-                sum += count(dice, i) * i;
-            }
-            Arrays.sort(scores);
-            this.score = (scores[5] == 3 && scores[4] == 2) ? sum : 0;
-        }
-        else if (yachtCategory == YachtCategory.FOUR_OF_A_KIND) {
-            int[] scores = new int[6];
-            int sum = 0;
-            for (int i=1; i<=6; i++) {
-                scores[i-1] = count(dice, i);
-                if (count(dice, i) >= 4) sum += i * 4;
-            }
-            Arrays.sort(scores);
-            this.score = (scores[5] >= 4) ? sum : 0;
-        }
-        else if (yachtCategory == YachtCategory.LITTLE_STRAIGHT) {
-            Arrays.sort(dice);
-            int[] straight = new int[]{1, 2, 3, 4, 5};
-            this.score = (Arrays.equals(dice, straight)) ? 30 : 0;
-        }
-        else if (yachtCategory == YachtCategory.BIG_STRAIGHT) {
-            Arrays.sort(dice);
-            int[] straight = new int[]{2, 3, 4, 5, 6};
-            this.score = (Arrays.equals(dice, straight)) ? 30 : 0;
-        }
-        else {
-            int sum = 0;
-            for (int d : dice) {
-                this.score += d;
-            }
+        switch (yachtCategory) {
+            case ONES:
+                this.score = countAndSum(dice, 1);
+                break;
+            case TWOS:
+                this.score = countAndSum(dice, 2);
+                break;
+            case THREES:
+                this.score = countAndSum(dice, 3);
+                break;
+            case FOURS:
+                this.score = countAndSum(dice, 4);
+                break;
+            case FIVES:
+                this.score = countAndSum(dice, 5);
+                break;
+            case SIXES:
+                this.score = countAndSum(dice, 6);
+                break;
+            case FULL_HOUSE:
+                int[] counted = count(dice);
+                Arrays.sort(counted);
+                this.score = (Arrays.equals(counted, FULL_HOUSE_FREQ))? 
+                    Arrays.stream(dice).sum() : 0;
+                break;
+            case FOUR_OF_A_KIND:
+                int[] four = count(dice);
+                Arrays.sort(dice);
+                this.score = (Arrays.stream(four)
+                        .anyMatch(n -> n>=4)) ?
+                    dice[2] * 4 : 0;
+                break;
+            case LITTLE_STRAIGHT:
+                this.score = (Arrays.stream(dice).sum() == 15)? 30: 0;
+                break;
+            case BIG_STRAIGHT:
+                this.score = (Arrays.stream(dice).sum() == 20)? 30: 0;
+                break;
+            case YACHT:
+                this.score = (Arrays.stream(dice).allMatch(n -> n==dice[0]))? 50: 0;
+                break;
+            default:
+                this.score = Arrays.stream(dice).sum();
         }
     }
 
@@ -67,23 +55,17 @@ class Yacht {
         return this.score;
     }
 
-    private int yacht(int[] dice) {
-        int first = dice[0];
-
-        int count = 0;
-        for (int d : dice) {
-            if (d == first) count++;
-        }
-
-        return (count == 5) ? 50: 0;
+    private int countAndSum(int[] dice, int counted) {
+        return Arrays.stream(dice)
+            .filter(d -> d == counted)
+            .sum();
     }
 
-    private int count(int[] dice, int counted) {
-        int count = 0;
-        for (int d : dice) {
-            if (d == counted) count++;
-        }
-
-        return count;
+    private int[] count(int[] dice) {
+        int[] counted = new int[6];
+        Arrays.fill(counted, 0);
+        Arrays.stream(dice)
+            .forEach(d -> counted[d - 1]+=1);
+        return counted;
     }
 }
