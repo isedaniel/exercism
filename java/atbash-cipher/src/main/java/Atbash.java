dc84f77978ac4a738116843a00d200030a4ee570
Daniel Ise <ise.daniel@gmail.com>
2024-06-24 14:04:28 -0300

    atbash done

--- java/atbash-cipher/src/main/java/Atbash.java
+++ java/atbash-cipher/src/main/java/Atbash.java
@@ -1,11 +1,38 @@
 class Atbash {
 
     String encode(String input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        input = input.toLowerCase();
+
+        var count = 0;
+        var sb = new StringBuilder();
+        for (char c : input.toCharArray()) {
+            if ('a' <= c && c <= 'z') {
+                sb.append((char) ('a' + 'z' - c));
+                count++;
+            } else if ('1' <= c && c <= '9') {
+                sb.append(c);
+                count++;
+            }
+
+            if (count == 5) {
+                sb.append(' ');
+                count = 0;
+            }
+        }
+        return sb.toString().trim();
     }
 
     String decode(String input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        input = input.toLowerCase();
+
+        var sb = new StringBuilder();
+        for (char c : input.toCharArray()) {
+            if ('a' <= c && c <= 'z')
+                sb.append((char) ('a' + 'z' - c));
+            else if ('1' <= c && c <= '9') 
+                sb.append(c);
+        }
+        return sb.toString();
     }
 
 }
