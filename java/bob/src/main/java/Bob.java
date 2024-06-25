168bdb48302125fcccc1b490ca75ba9fd31be5d6
Daniel Ise <ise.daniel@gmail.com>
2024-06-25 14:01:08 -0300

    bob done

--- java/bob/src/main/java/Bob.java
+++ java/bob/src/main/java/Bob.java
@@ -1,7 +1,32 @@
 class Bob {
 
-    String hey(String input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    String hey(String s) {
+        s = s.trim();
+        if (s.isEmpty()) {
+            return "Fine. Be that way!";
+        }
+        else if (yell(s) && question(s)) {
+            return "Calm down, I know what I'm doing!";
+        } else if (question(s)) {
+            return "Sure.";
+        } else if (yell(s)) {
+            return "Whoa, chill out!";
+        }
+        return "Whatever.";
+    }
+
+    boolean yell(String s) {
+        boolean hasLetter = s.matches(".*[a-zA-Z]+.*");
+        for (char c : s.toCharArray()) {
+            if (Character.isLowerCase(c)) {
+                return false;
+            }
+        }
+        return hasLetter;
+    }
+
+    boolean question(String s) {
+        return s.charAt(s.length() - 1) == '?';
     }
 
 }
